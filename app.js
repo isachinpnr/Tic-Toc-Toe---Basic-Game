e6e513a6367c391c0dc2db2d0bcf0a616d37f929
isachinpnr <uttarakhandtechnology@gmail.com>
2024-10-03 23:59:39 +0530

    add Tic Toc Toe all files in Github

---
+++ app.js
@@ -0,0 +1,78 @@
+let boxes = document.querySelectorAll(".box");
+let resetBtn = document.querySelector("#resetBtn");
+let newBtn = document.querySelector("#new-Btn");
+let msg = document.querySelector("#msg");
+let msgContainer = document.querySelector(".msg-Container");
+
+
+let turnO = true; //playerO turn....
+let turnX = false;
+
+const winPatterns = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [3, 4, 6]
+];
+
+const resetGame = () => {
+    turnO = true;
+    enabledBoxes();
+    msgContainer.classList.add("hide");
+}
+
+boxes.forEach((box) => {
+    box.addEventListener("click", () => {
+        if (turnO) {
+            //playerO
+            box.innerText = "O";
+            turnO = false;
+            box.style.color = "blue";
+        } else{
+            //playerX
+            box.innerText = "X";
+            turnO = true;
+        }
+        box.disabled = true;
+
+        checkWinner();
+    });
+});
+
+const disabledBoxes = () => {
+    for (let box of boxes) {
+        box.disabled = true;
+    }
+}
+const enabledBoxes = () => {
+    for (let box of boxes) {
+        box.disabled = false;
+        box.innerText = "";
+    }
+}
+const showWinner = (winner) => {
+    msg.innerText = `Congratulation, winner is ${winner}`;
+    msgContainer.classList.remove("hide");
+    disabledBoxes();
+};
+
+const checkWinner = () => {
+    for (let pattern of winPatterns) {
+        let pos1Val = boxes[pattern[0]].innerText;
+        let pos2Val = boxes[pattern[1]].innerText;
+        let pos3Val = boxes[pattern[2]].innerText;
+
+        if (pos1Val != "" && pos2Val != "" && pos3Val != "") {
+            if (pos1Val === pos2Val && pos2Val === pos3Val) {
+                showWinner(pos1Val);
+            }
+        }
+    }
+}
+
+newBtn.addEventListener("click", resetGame);
+resetBtn.addEventListener("click", resetGame);
